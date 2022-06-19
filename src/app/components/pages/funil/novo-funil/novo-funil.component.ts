797aba07fe1d0102fb88a4eaade9b487ba119b8e
Gustavo Alves Leite <gustavoyodane@hotmail.com>
2022-06-18 23:33:12 -0300

    add-delete-fasesFunil

--- src/app/components/pages/funil/novo-funil/novo-funil.component.ts
+++ src/app/components/pages/funil/novo-funil/novo-funil.component.ts
@@ -3,12 +3,16 @@ import { Funil } from 'src/app/interfaces/funil';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { AngularFirestore } from "@angular/fire/compat/firestore";
 import { IdeaService } from 'src/app/service/idea.service';
+import { isNgTemplate } from '@angular/compiler';
 @Component({
   selector: 'app-novo-funil',
   templateUrl: './novo-funil.component.html',
   styleUrls: ['./novo-funil.component.scss']
 })
 export class NovoFunilComponent implements OnInit {
+
+
+  arrayFase:any = []
 
   //IMPORTANDO DADOS DA INTERFACE
   public Dadosfunil: Funil = {};
@@ -18,15 +22,32 @@ export class NovoFunilComponent implements OnInit {
     public service: IdeaService
   ) { }
 
+
   ngOnInit(): void {
   }
 
+
+  criarFase(){
+
+          this.arrayFase.push(this.Dadosfunil.fase)
+          console.log(this.arrayFase)
+  }
+  deleteFase(item: any){
+    console.log(item)
+    let index = this.arrayFase.indexOf(item)
+    if(index > -1){
+       this.arrayFase.splice(index,1)
+    }
+
+  console.log(this.arrayFase)
+
+  }
   addFunil(){
 
     let id = this.fireservice.createId();
     let nome = this.Dadosfunil.nome;
     let empresa = this.Dadosfunil.empresa;
-    let fase = this.Dadosfunil.fase;
+    let fase = this.arrayFase;
 
      let dados = [
          {
@@ -39,5 +60,8 @@ export class NovoFunilComponent implements OnInit {
      console.log(dados[0])
      this.service.criarFunil(id, dados[0]);
   }
+
+
+
 
 }
--- src/app/interfaces/funil.ts
+++ src/app/interfaces/funil.ts
@@ -2,6 +2,6 @@ export interface Funil {
   nome?: string | undefined;
   empresa?: string | undefined;
   id?: string | undefined;
-  fase?: string | undefined;
+  fase?: string[] | undefined;
 
 }
