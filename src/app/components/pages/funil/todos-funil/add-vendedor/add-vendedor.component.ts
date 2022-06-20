c283899b1c8bf3589c5d04a7b2eae309e30bb35c
Marcos Ribeiro <63163857+marcosviniweb@users.noreply.github.com>
2022-06-20 12:16:09 -0300

    Vendedor

--- src/app/components/pages/funil/todos-funil/add-vendedor/add-vendedor.component.ts
+++ src/app/components/pages/funil/todos-funil/add-vendedor/add-vendedor.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { IdeaService } from 'src/app/service/idea.service';
 
 @Component({
   selector: 'app-add-vendedor',
@@ -10,7 +13,22 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 export class AddVendedorComponent implements OnInit {
 
   add = faPlus
-  constructor() { }
+  id: string = '';
+  funil: any;
+  arrayFase: any;
+  constructor(
+    public fireservice: AngularFirestore,
+    public service: IdeaService,
+    private router: Router,
+    private activeRoute: ActivatedRoute
+
+  ) {
+    this.id = this.activeRoute.snapshot.params['id'];
+    this.service.getFunilId(this.id ).subscribe(res =>{
+     this.funil = res;
+         console.log(res)
+   })
+   }
 
   ngOnInit(): void {
   }
